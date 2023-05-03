3cb80b451675153dcb3512a03058ee5b6c30915d
VankataLv <132002763+VankataLv@users.noreply.github.com>
2023-05-03 20:03:38 +0300

    First part of problem 4

--- 5. Lists - Basics  - Exercise/4. Number Beggars.py
+++ 5. Lists - Basics  - Exercise/4. Number Beggars.py
@@ -1,23 +1,14 @@
-first_line = input()
-money = first_line.split(", ")
+coins = input().split(", ")
 beggars = int(input())
 beggar_earnings = []
 current_beggar_earnings = 0
-old_list = list()
-if len(money) > beggars:
+
+if len(coins) > beggars:
     for beggar in range(beggars):
-        for i in range(0 + beggar, len(money), len(money) // 2):
-            current_beggar_earnings += int(money[i])
+        for i in range(0 + beggar, len(coins), len(coins) // 2):
+            current_beggar_earnings += int(coins[i])
         beggar_earnings.insert(beggar, current_beggar_earnings)
         current_beggar_earnings = 0
 
-elif len(money) == beggars:
-    for i_value in money:
-        beggar_earnings.append(int(i_value))
 
-else:
-    for i_value in money:
-        old_list.append(int(i_value))
-    for _ in range(0, beggars):
-        beggar_earnings.append(sum(old_list[_::beggars]))
 print(beggar_earnings)
