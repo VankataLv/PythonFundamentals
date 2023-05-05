b6a61e0632162b6f00b5a97ac7096a29bf9b035f
VankataLv <132002763+VankataLv@users.noreply.github.com>
2023-05-05 08:46:43 +0300

    problem 4  fixed two issues now (80/100)

--- 5. Lists - Basics  - Exercise/4. Number Beggars.py
+++ 5. Lists - Basics  - Exercise/4. Number Beggars.py
@@ -5,7 +5,7 @@ current_beggar_earnings = 0
 
 if len(coins) > beggars:
     for beggar in range(beggars):
-        for i in range(0 + beggar, len(coins), len(coins) // 2):
+        for i in range(0 + beggar, len(coins), beggars):
             current_beggar_earnings += int(coins[i])
         beggar_earnings.insert(beggar, current_beggar_earnings)
         current_beggar_earnings = 0
@@ -14,7 +14,7 @@ if len(coins) > beggars:
 elif len(coins) == beggars:
     print([int(coin) for coin in coins])
 
-else:
+elif len(coins) < beggars:
     for insufficient_coins in range(beggars - len(coins)):
         coins.append(str(0))
         print([int(coin) for coin in coins])
