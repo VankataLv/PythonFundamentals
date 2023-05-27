06b81c9ef81a0770d0a90b8131c195f8ed259c37
VankataLv <132002763+VankataLv@users.noreply.github.com>
2023-05-27 15:29:32 +0300

    Mid-term exams

--- Mid-exam-prep/04/01. Guinea Pig.py
+++ Mid-exam-prep/04/01. Guinea Pig.py
@@ -13,3 +13,4 @@ if qty_food >= 0 and qty_cover >= 0 and qty_hay >= 0:
         f'Everything is fine! Puppy is happy! Food: {qty_food / 1000:.2f}, Hay: {qty_hay / 1000:.2f}, Cover: {qty_cover / 1000:.2f}.')
 else:
     print("Merry must go to the pet store!")
+
