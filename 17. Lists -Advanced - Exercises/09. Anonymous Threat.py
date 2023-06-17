4e53cf85a49f57db81518e8c4bed277000d6b01d
VankataLv <132002763+VankataLv@users.noreply.github.com>
2023-06-17 09:03:25 +0300

    problem 9 - finally 100/100 - anon threat !!!!

--- 17. Lists -Advanced - Exercises/09. Anonymous Threat.py
+++ 17. Lists -Advanced - Exercises/09. Anonymous Threat.py
@@ -1,29 +1,44 @@
-data = input().split(" ")
+from math import floor
+from math import ceil
+data = [el for el in input().split(" ")]
 command = input()
-new_value = ""
 while command != "3:1":
-    if command[0:5] == "merge":
-        action, start_index, end_index = command.split(" ")
-        start_index = int(start_index)
-        end_index = int(end_index)
-        if start_index not in range(0, len(data)) or end_index not in range(0, len(data)):
-            if start_index > len(data) - 1:
-                start_index = end_index = len(data) - 1
-            elif end_index > len(data) - 1:
-                end_index = len(data) - 1
-        for i_to_merge in range(start_index, end_index + 1):
-            new_value += data[i_to_merge]
-        for i_to_delete in range(end_index, start_index - 1, -1):
-            data.pop(i_to_delete)
-        data.insert(0, new_value)
-        new_value = ""
-        print(data)
-    elif command[0:6] == "divide":
-        action, index, partitions = command.split(" ")
+    action, first, second = command.split(" ")
+    if action == "merge":
+        start_index = int(first)
+        end_index = int(second)
+        start_index = max(0, start_index)
+        end_index = min(len(data) - 1, end_index)
+        merged = ""
+        for i in range(start_index, end_index + 1):
+            merged += data[start_index]
+            data.pop(start_index)
+        data.insert(start_index, merged)
+        merged = ""
+    elif action == "divide":
+        divided_el = []
+        index = int(first)
+        partitions = int(second)
+        el_to_divide = data[index]
+        data.pop(index)
+        if len(el_to_divide) % partitions == 0:
+            symbols = len(el_to_divide) // partitions
+            for partition in range(partitions):
+                divided_el.insert(partition, el_to_divide[0:symbols])
+                el_to_divide = el_to_divide[symbols:]
+            for el in reversed(divided_el):
+                data.insert(index, el)
+            divided_el = []
+        else:
+            last_el_length = ceil(len(el_to_divide) / partitions)
+            other_el_length = floor(len(el_to_divide) / partitions)
+            for partition in range(partitions - 1):
+                divided_el.insert(partition, el_to_divide[0:other_el_length])
+                el_to_divide = el_to_divide[other_el_length:]
+            divided_el.append(el_to_divide)
+            for el in reversed(divided_el):
+                data.insert(index, el)
+            divided_el = []
     command = input()
 
-# Ivo Johny Tony Bony Mony
-# merge 0 3
-# merge 3 4
-# merge 0 3
-# 3:1
+print(" ".join(data))
