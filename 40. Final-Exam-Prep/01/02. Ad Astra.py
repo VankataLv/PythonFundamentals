4c272caf7d8ee10d8769a2300def33b49013244d
VankataLv <132002763+VankataLv@users.noreply.github.com>
2023-07-01 09:04:52 +0300

    Final exam prep

--- 40. Final-Exam-Prep/01/02. Ad Astra.py
+++ 40. Final-Exam-Prep/01/02. Ad Astra.py
@@ -4,8 +4,7 @@ cur_item = {}
 txt = input()
 lst_calories = []
 calories = 0
-pattern = r"(\||#)(?P<name>[A-Za-z\s]+)(\||#)(?P<expdate>\d{2}/\d{2}/\d{2})(\||#)(?P<calories>\d+)(\||#)"
-pattern2 = r"(\||#)([A-Za-z\s]+)(\||#)(?P<expdate>\d{2}/\d{2}/\d{2})(\||#)(?P<calories>\d+)(\||#)"
+pattern = r"(?P<sep>\||#)(?P<name>[A-Za-z\s]+)(\1)(?P<expdate>\d{2}/\d{2}/\d{2})(\1)(?P<calories>\d+)(\1)"
 food_stores = re.finditer(pattern, txt)
 lst_calories = re.findall(pattern, txt)
 for i in lst_calories:
@@ -14,5 +13,3 @@ print(f"You have food to last you for: {floor(calories / 2000)} days!")
 for item in food_stores:
     cur_item = item.groupdict()
     print(f"Item: {cur_item['name']}, Best before: {cur_item['expdate']}, Nutrition: {cur_item['calories']}")
-
-# 66/100
